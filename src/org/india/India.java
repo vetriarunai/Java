272b1bceffd6131d5141f792fa9169e0f3c927ac
vetriarunai <vetriarunai@gmail.com>
2022-09-04 18:38:08 +0530

    updated

--- src/org/india/India.java
+++ src/org/india/India.java
@@ -1,14 +1,21 @@
 package org.india;
 
 import org.tamilnadu.Tamilnadu;
+
 //child
 public class India extends Tamilnadu {
+	public void a(){
+		System.out.println("Trichy");
+	}
+
 	private void india() {
 		System.out.println("Indian");
 	}
 
 	public static void main(String[] args) {
+
 		India s = new India();
+		s.a();
 		s.india();
 		s.malayalam();
 		s.tamilLanguage();
